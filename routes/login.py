0ac1a8881590ae1d44d02e3aaa63b988fc7376ca
Ruan Rodrigues <106561796+rodriguezruan@users.noreply.github.com>
2023-11-30 07:23:57 -0500

    Update login.py

--- routes/login.py
+++ routes/login.py
@@ -1,14 +1,12 @@
 from flask import Blueprint, request, jsonify
 
-#login_bp = Blueprint('login', __name__)
 
-
-@login_mongo.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def fazer_login():
     data = request.get_json()
-    email = data['email']
+    username = data['username']
     senha = data['senha']
-    usuario = users_collection.find_one({'email': email})
+    usuario = users_collection.find_one({'username': username})
     if usuario and check_password_hash(usuario['senha'], senha):
         return jsonify({'mensagem': 'Login feito com sucesso!!!'}), 200
     return jsonify({'mensagem': 'Senha ou Email inválidos'}), 401
